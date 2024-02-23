a5554e9f703d814cca3195591586817c4135172b
Axel Aguilar Alonso <agui.axel11@gmail.com>
2024-02-23 16:26:41 -0700

    Create ImageBuilder.cs
    For build SQL with BASE64

---
+++ APPADMON001SM/APPADMONAPI001/Data/BDAdmon/ImageBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.BDAdmon
+{
+    public class ImageBuilder
+    {   
+        private byte[] Picture { get; set; }
+        
+        public ImageBuilder() {
+            
+        }
+        
+        public byte [] buildSQLImage(string Image)
+        {
+            string suffixToFind = ";base64,";
+            int suffixIndex = Image.LastIndexOf(suffixToFind, StringComparison.OrdinalIgnoreCase);
+            if (suffixIndex != -1)
+            {
+                Image = Image.Substring(suffixIndex + suffixToFind.Length);
+
+            }
+            return Convert.FromBase64String(Image);
+        }
+    }
+}
