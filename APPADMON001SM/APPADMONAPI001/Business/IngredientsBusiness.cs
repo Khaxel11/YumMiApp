55c3decfd2357b5c5eee219bcf6b8a69b294395b
Manuel Valenzuela <manuel.valenzuela@ciipdetec.com>
2024-08-27 10:32:50 -0700

    Catalogo de Ingredientes
    Se creo Catalogo de Ingredientes

---
+++ APPADMON001SM/APPADMONAPI001/Business/IngredientsBusiness.cs
@@ -0,0 +1,44 @@
+﻿using Data;
+using Entity;
+using Entity.DTO.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class IngredientsBusiness
+    {
+        /*
+        * Author:      Manuel de Jesus Valenzuela Gracian
+        * Date:        27Ago2024
+        * Descripción: Métodos para ABC de ingrendientes ("CRUD" = CREATE, READ, UPDATE AND DELETE)S
+        */
+
+        public async Task<Result> getIngredients(TokenData DatosToken, string Filtro)
+        {
+            try
+            {
+                return await new IngredientsData().getIngredients(DatosToken, Filtro);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+
+        public async Task<Result> controlIngredients(TokenData DatosToken, int Opcion, IngredientsEntity model)
+        {
+            try
+            {
+                return await new IngredientsData().controlIngredients(DatosToken, Opcion, model);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ APPADMON001SM/APPADMONAPI001/Entity/IngredientsEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+
+    /*
+    * Author:      Manuel de Jesus Valenzuela Gracian
+    * Date:        27Ago2024
+    * Descripción: Entidad(s) o Modelo(s) para catalogo Ingrendients
+    */
+
+    public class IngredientsEntity
+    {
+
+        public int idIngrediente { get; set; }
+        public string ingrediente { get; set; }
+        public string descripcion { get; set; }
+        public byte[] foto { get; set; }
+        public string stringFoto { get; set; }
+        public bool activo { get; set; }
+        //public DateTime fechaInsert { get; set; }
+        //public DateTime fechaUpdate { get; set; }
+        //public DateTime fechaDelete { get; set; }
+        //public string usuarioInsert { get; set; }
+        //public string usuarioUpdate { get; set; }
+        //public string usuarioDelete { get; set; }
+    }
+}
